13b2c18c2ab8f0650ebca0523293489754512fa2
krish101106 <krishgajjar051@gmail.com>
2026-07-15 01:08:54 +0530

    Add currency converter React project
    Initialize new React + Vite application for currency conversion. Includes Tailwind CSS for styling, Oxlint for linting, and modern React 19 setup with development and build scripts.

---
+++ currency/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-black-400'>Chai and krish</h1>
+    </>
+  )
+}
+
+export default App
